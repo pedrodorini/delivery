30e6eaf4b143f88af2711c95f2c3f6557b92ece4
Andre Tosetto <andreftosetto@gmail.com>
2017-11-29 21:45:56 -0200

    arrumando o botao restaurant

--- delivery/src/app/app.module.ts
+++ delivery/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ROUTES} from './app.routes'
 import { AppComponent } from './app.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [
     AppComponent,
     RestaurantsComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
