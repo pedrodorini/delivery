70ad77013f450e9ce628415daedb1e3fb1fda4ba
AdrielBernal <dry_b@hotmail.com>
2017-12-05 23:25:17 -0200

    Correções shopping Cart

--- delivery/src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ delivery/src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MenuItem } from './menu-item.model';
 
 @Component({
@@ -8,10 +8,14 @@ import { MenuItem } from './menu-item.model';
 export class MenuItemComponent implements OnInit {
 
   @Input() menuItem: MenuItem
-  
+  @Output() add = new EventEmitter()
+
   constructor() { }
 
   ngOnInit() {
   }
+  emitAddEvent(){
+   this.add.emit(this.menuItem)
+ }
 
 }
